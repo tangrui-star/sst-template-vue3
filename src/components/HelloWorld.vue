8494fb17a2f72235ba961ebbb4cfa167b3fd0a67
tangrui <tangrui@si-tech.com.cn>
2024-12-11 16:26:51 +0800

    sst-template-vue3 init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    sst-template-vue3
+  </div>
+</template>
+
+<style scoped>
+</style>
